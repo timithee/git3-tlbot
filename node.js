60d6f06dcfdc1b8bcada6fa1be5f88a51e3b9bb5
timithee <momingym@gmail.com>
2024-06-13 11:48:04 +0900

    Update node.js

--- node.js
+++ node.js
@@ -1,7 +1,7 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const { Guilds, GuildMessages, MessageContent } = GatewayIntentBits;
 const client = new Client({ intents: [Guilds, GuildMessages, MessageContent] });
-client.login('MTI1MDAzODU1OTg2ODU4NDAzOA.GLULvN.xY2Po31Z6t6miTJUYRAP4PtJe60TCVZlk8IhxY');
+client.login('token');
 
 client.once('ready', () => console.log(client.user.tag+' 준비 완료!'));
 
